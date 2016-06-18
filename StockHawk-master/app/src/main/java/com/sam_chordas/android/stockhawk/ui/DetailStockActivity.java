2b8aef7b1863b9aea3fcfa812e3cc4b7f312af45
Mangesh <mangesh.kadam@citruspay.com>
2016-06-18 19:23:28 +0530

    * added chart foe stock details.

---
+++ StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailStockActivity.java
@@ -0,0 +1,117 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.db.chart.Tools;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.AxisController;
+import com.db.chart.view.LineChartView;
+import com.db.chart.view.animation.Animation;
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+public class DetailStockActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+
+    public static final String STOCK_INFO = "STOCK_INFO";
+
+    private String stock_info;
+    private LineChartView lineChartView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_graph);
+
+        stock_info = getIntent().getStringExtra(STOCK_INFO);
+
+        setTitle(stock_info);
+        lineChartView = (LineChartView) findViewById(R.id.linechart);
+
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case 0:
+                return new CursorLoader(this, QuoteProvider.Quotes.CONTENT_URI,
+                        new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
+                                QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP},
+                        QuoteColumns.SYMBOL + " = ?",
+                        new String[]{stock_info},
+                        null);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data.getCount() != 0) {
+            displayChart(data);
+          } else {
+            Toast.makeText(this, getString(R.string.no_stock_error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void displayChart(Cursor data) {
+        LineSet lineSet = new LineSet();
+        float minimumPrice = 0;
+        float maximumPrice = 0;
+
+        for (data.moveToFirst(); !data.isAfterLast(); data.moveToNext()) {
+            String label = data.getString(data.getColumnIndexOrThrow(QuoteColumns.BIDPRICE));
+            float price = Float.parseFloat(label);
+
+            lineSet.addPoint(label, price);
+            if (minimumPrice == 0) {
+                minimumPrice = price;
+            } else {
+                Math.min(minimumPrice, price);
+            }
+
+            if (maximumPrice == 0) {
+                maximumPrice = price;
+            } else {
+                Math.max(maximumPrice, price);
+            }
+        }
+
+        lineSet.setColor(Color.parseColor("#53c1bd"))
+                .setFill(Color.parseColor("#3d6c73"))
+                .setDotsColor(Color.parseColor("#758cbb"))
+                .setThickness(4)
+                .setDashed(new float[]{10f, 10f});
+
+
+        lineChartView.setBorderSpacing(Tools.fromDpToPx(15))
+                .setYLabels(AxisController.LabelPosition.OUTSIDE)
+                .setXLabels(AxisController.LabelPosition.NONE)
+                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setXAxis(false)
+                .setYAxis(false)
+                .setAxisBorderValues(Math.round(minimumPrice - 10f), Math.round(maximumPrice + 10f))
+                .addData(lineSet);
+
+        Animation anim = new Animation();
+
+        lineChartView.show(anim);
+    }
+
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+}
